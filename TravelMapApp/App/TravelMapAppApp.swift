ad3daeb3ed44c76e301b4663b13743dd99efef11
Emirhan Zeyrek <contactemirhanzeyrek@gmail.com>
2025-02-12 19:23:48 +0300

    First Commit

---
+++ TravelMapApp/App/TravelMapAppApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct TravelMapAppApp: App {
+    @StateObject private var vm = LocationsViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            LocationsView()
+                .environmentObject(vm)
+        }
+    }
+}
